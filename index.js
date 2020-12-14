bd3530b44e6eec68082d0088b76fd54cd00c76cd
armaghan05 <72584622+armaghan05@users.noreply.github.com>
2020-12-14 13:08:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+var DiceSound = new Audio ("sounds/dice.mp3");
 // ENTER NAMES:
-
 var p1Name = prompt("Please enter 1st player's name");
-var p2Name = prompt ("Please enter 2nd player's name");
+var p2Name = prompt ("Please enter 2nd player's named");
 
 document.querySelector(".player1").innerHTML = p1Name;
 document.querySelector(".player2").innerHTML = p2Name;
@@ -11,7 +11,10 @@ document.querySelector(".player2").innerHTML = p2Name;
      var changeText =
 } */
 //==============================================================
+document.querySelector(".dodo").addEventListener("click", play);
+
 function play() {
+DiceSound.play();
 // FOR DICE ONE:
 var randomNumber1 = (Math.floor(Math.random() * 6 ) + 1);
 
